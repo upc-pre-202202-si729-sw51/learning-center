a6e51f153bf205cc116fced7b7c4d1b91fd0f9a3
Jorge Mayta <jmayta@pucp.edu.pe>
2022-09-14 19:08:54 -0500

    feat(students): Added Students Management View

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StudentsComponent} from "./learning/pages/students/students.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'students', component: StudentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
